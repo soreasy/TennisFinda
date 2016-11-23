9396e4caa4efd0a8c12d4594abee1919ddd7b525
Soren Zeliger <szeliger@oberlin.edu>
2016-11-23 18:36:50 -0500

    retrieves lat/lng for inputted zip. setState not having a permanent effect for some reason

--- app/assets/javascripts/components/MotherApp.es6.jsx
+++ app/assets/javascripts/components/MotherApp.es6.jsx
@@ -1,23 +1,54 @@
 class MotherApp extends React.Component {
     constructor() {
         super()
+        this.handleSubmit = this.handleSubmit.bind(this)
+
+        this.state = {
+            lat: null,
+            lng: null
+        }
+    }
+
+    handleSubmit(event) {
+        let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${$('#zip').val()}&key=AIzaSyCGh4qceX9J_w0ZjqFG2WfScGlQ_7p7Uek`
+        fetch(url)
+            .then((response) => response.json())
+            .then((json_response) => {
+                let lat = json_response.results[0].geometry.location.lat
+                let lng = json_response.results[0].geometry.location.lng
+                this.setState({lat: lat, lng: lng})
+            }.bind(this))
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        debugger;
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        debugger;
     }
 
     render() {
+        var lng = this.state.lng
+
         return(
             <div className="row">
-                <form className="col s12">
+                <form className="col s12" onSubmit={this.handleSubmit}>
                   <div className="row">
                     <div className="input-field col s6">
-                      <input id="first_name" type="text" className="validate" />
+                      <input id="zip" type="text" className="validate" />
                       <label htmlFor="first_name">Zip Code</label>
                     </div>
                     <div className="input-field col s6">
-                      <input id="last_name" type="text" className="validate" />
+                      <input id="radius" type="text" className="validate" />
                       <label htmlFor="last_name">Max Radius</label>
                     </div>
                   </div>
+                  <button className="btn waves-effect waves-light" type="submit" name="action">Submit
+                    <i className="material-icons right">send</i>
+                  </button>
                 </form>
+                <p>Lng is {lng}</p>
             </div>
         )
     }
