9ed2cd171ad7c9b6e2432d99e875fad9eb2f8917
Soren Zeliger <szeliger@oberlin.edu>
2016-11-26 19:53:05 -0500

    makes fetch request for tennis courts within radius at supplied zip, but google places doesnt like CORS requests =(

--- app/assets/javascripts/components/MotherApp.es6.jsx
+++ app/assets/javascripts/components/MotherApp.es6.jsx
@@ -6,20 +6,30 @@ class MotherApp extends React.Component {
         this.state = {
             lat: null,
             lng: null,
-            bla: 1
+            courts: null
         }
     }
 
     handleSubmit(event) {
         event.preventDefault()
-        let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${$('#zip').val()}&key=AIzaSyCGh4qceX9J_w0ZjqFG2WfScGlQ_7p7Uek`
-        fetch(url)
+        let zip_to_latlng_url = `https://maps.googleapis.com/maps/api/geocode/json?address=${$('#zip').val()}&key=AIzaSyCGh4qceX9J_w0ZjqFG2WfScGlQ_7p7Uek`
+        fetch(zip_to_latlng_url)
             .then((response) => response.json())
             .then((json_response) => {
                 let lat = json_response.results[0].geometry.location.lat
                 let lng = json_response.results[0].geometry.location.lng
                 this.setState({lat: lat, lng: lng})
+                let meter_rad = Math.floor((+$('#radius').val() * 1609.34))
+                // debugger;
+                let courts_url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${this.state.lat},${this.state.lng}&radius=${meter_rad}&keyword=tennis&key=AIzaSyCGh4qceX9J_w0ZjqFG2WfScGlQ_7p7Uek`
+                fetch(courts_url, {mode: 'cors'})
+                    .then((response) => response.json())
+                    .then((json_response) => {
+                        debugger;
+                    }.bind(this))
             }.bind(this))
+
+
     }
 
     componentWillUpdate(nextProps, nextState) {
@@ -32,8 +42,6 @@ class MotherApp extends React.Component {
 
 
     render() {
-        var lng = this.state.lng
-
         return(
             <div className="row">
                 <form className="col s12" onSubmit={this.handleSubmit}>
@@ -44,15 +52,13 @@ class MotherApp extends React.Component {
                     </div>
                     <div className="input-field col s6">
                       <input id="radius" type="text" className="validate" />
-                      <label htmlFor="last_name">Max Radius</label>
+                      <label htmlFor="last_name">Max Radius (in miles)</label>
                     </div>
                   </div>
                   <button className="btn waves-effect waves-light" type="submit" name="action">Submit
                     <i className="material-icons right">send</i>
                   </button>
                 </form>
-                <div onClick={this.bla}>Click Me!</div>
-                <p>Lng is {lng}</p>
             </div>
         )
     }
